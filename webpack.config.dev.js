37d21923e21e67ed080afcc87f20c72237bfdda0
Robin Luiten <robin.luiten+git@gmail.com>
2016-07-21 22:29:19 +1000

    fix(webpack): make single config env aware
    fix(index.html): moved to src

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,6 +2,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 // import path from 'path';
 // import webpack from 'webpack';
@@ -18,7 +19,7 @@ const webpack = require('webpack');
 // 	}),
 // ],
 
-//-- Environment.
+// Environment.
 const NODE_ENV = process.env.NODE_ENV;
 
 const ENV_DEVELOPMENT = NODE_ENV === 'development';
@@ -26,59 +27,119 @@ const ENV_PRODUCTION = NODE_ENV === 'production';
 const ENV_TEST = NODE_ENV === 'test';
 
 const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-console.log('webpack.config.dev.js [process.env.NODE_ENV]', process.env.NODE_ENV);
+console.log('webpack environment', process.env.NODE_ENV);
 
+const loaders = {
+	js: {
+		test: /\.js|\.jsx$/,
+		loaders: ['babel'],
+		include: path.join(__dirname, 'src')
+	}
+	// scss: {test: /\.scss$/, loader: 'style!css!postcss!sass'}
+};
+
+const preLoaders = {
+	js: {
+		test: /\.js|\.jsx$/,
+		loaders: ['eslint-loader'],
+		exclude: ['node_modules']
+	}
+};
+
+// Config
 const config = {};
 module.exports = config;
 
-if (ENV_DEVELOPMENT) {
-	Object.assign(config, {
-		devServer: {
-			// same path as build convenient have only one index.html setup then.
-			contentBase: 'static/',
-		},
-	});
-}
+config.resolve = {
+	extensions: ['', '.js', '.jsx']
+	// modulesDirectories: ['node_modules'],
+	// root: path.resolve('.')
+};
 
-// if (ENV_DEVELOPMENT || ENV_PRODUCTION) {
-//
-// }
+config.plugins = [
+	new webpack.DefinePlugin({
+		'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+	})
+];
 
-// export default {
-Object.assign(config, {
-	devtool: 'cheap-module-eval-source-map',
 
-	entry: [
-		'./src/index'
-	],
-	output: {
-		path: path.join(__dirname, 'static', 'scripts'),
+if (ENV_DEVELOPMENT || ENV_PRODUCTION) {
+	config.entry = {
+		main: ['./src/index']
+	};
+
+	config.output = {
+		path: path.join(__dirname, 'static'),
 		filename: 'bundle.js',
 		publicPath: '/'
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+	};
+
+	config.plugins.push(
+		new HtmlWebpackPlugin({
+			chunkSortMode: 'dependency',
+			filename: 'index.html',
+			hash: false,
+			inject: 'body',
+			template: './src/index.html'
 		})
-	],
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	eslint: {
+	);
+
+	config.module = {
+		preLoaders: [ preLoaders.js ],
+		loaders: [ loaders.js ]
+	};
+
+	config.eslint = {
 		configFile: '.eslintrc'
-	},
-	module: {
-		preLoaders: [{
-			test: /\.js|\.jsx$/,
-			loaders: ['eslint-loader'],
-			exclude: ['node_modules']
-		}],
-		loaders: [{
-			test: /\.js|\.jsx$/,
-			loaders: ['babel'],
-			include: path.join(__dirname, 'src')
-		}]
-	}
-});
+	};
+
+}
+
+if (ENV_DEVELOPMENT) {
+	config.devtool = 'cheap-module-source-map';
+
+	config.entry.main.unshift(
+		`webpack-dev-server/client?http://${HOST}:${PORT}`,
+		// 'webpack/hot/only-dev-server',
+		// 'react-hot-loader/patch',
+		'babel-polyfill'
+	);
+}
+
+if (ENV_PRODUCTION) {
+	config.devtool = 'source-map';
+
+	config.output.filename = '[name].[chunkhash].js';
+
+	// Over ride production public path
+	// so can access last production build in path /static/ under dev server
+	config.output.publicPath = '/static/';
+
+	config.module = {
+		loaders: [
+			loaders.js
+			// {test: /\.scss$/, loader: ExtractTextPlugin.extract('css?-autoprefixer!postcss!sass')}
+		]
+	};
+
+	config.plugins.push(
+		new WebpackMd5Hash(),
+		// new ExtractTextPlugin('styles.[contenthash].css'),
+		// new webpack.optimize.CommonsChunkPlugin({
+		// 	name: 'vendor',
+		// 	minChunks: Infinity
+		// }),
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			mangle: true,
+			compress: {
+				dead_code: true, // eslint-disable-line camelcase
+				screw_ie8: true, // eslint-disable-line camelcase
+				unused: true,
+				warnings: false
+			}
+		})
+	);
+}

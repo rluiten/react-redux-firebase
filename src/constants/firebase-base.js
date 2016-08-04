64149e14fb6a027f69c5227cdd21543fc4a8c750
Robin Luiten <robin.luiten+git@gmail.com>
2016-08-04 21:08:50 +1000

    fix(firebase): reset config, old config deactivated
    (cherry picked from commit 2fdd73b67a1f7b6dcdf9420fc15edb35823f4bb2)

---
+++ src/constants/firebase-base.js
@@ -0,0 +1,8 @@
+export default {
+	firebaseConfig: {
+		apiKey: 'AIzaSyB-QLyZVy9JvMX_rv54Fc9YgFaUJqA2kHw',
+		authDomain: 'fir-2-d8645.firebaseapp.com',
+		databaseURL: 'https://fir-2-d8645.firebaseio.com',
+		storageBucket: 'fir-2-d8645.appspot.com'
+	}
+};
